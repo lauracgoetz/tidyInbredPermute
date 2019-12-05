5e0df05209cf25fba2ce0220cab47616f0acdbad
Eric C. Anderson <eric.anderson@noaa.gov>
2019-12-05 12:17:48 -0700

    Initial commit.  (But I am pretty much done with it all)

---
+++ R/import.R
@@ -0,0 +1,17 @@
+
+#### Import the pipe operator from magrittr ####
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+
+#### Some focused imports
+#' @importFrom dplyr  case_when filter mutate select
+#' @importFrom string str_detect
